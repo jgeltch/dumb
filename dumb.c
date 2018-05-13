b06ba5a351c28f81bc48873fbdef96a344b23676
wildbartty <tominoz99@gmail.com>
2018-05-13 23:40:54 +1000

    added time to c implemetation

--- dumb.c
+++ dumb.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 
 int main () {
+  clock_t start = clock(), diff;
   long limit = 1l << 32;
   for (unsigned long i = 0; i < limit; i++) {
     if (i % 1000000 == 0) {
@@ -9,6 +10,10 @@ int main () {
     }
   }
 
+  diff = clock() - start;
+
+  int msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
   return 0;
   
 }
