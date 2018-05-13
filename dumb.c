ca96aa68ee81d81157f7ef7b7efd607e38016316
wildbartty <tominoz99@gmail.com>
2018-05-13 23:29:36 +1000

    added dumb.c

---
+++ dumb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+  long limit = 1l << 32;
+  for (unsigned long i = 0; i < limit; i++) {
+    if (i % 1000000 == 0) {
+      printf("%u\n", i);
+    }
+  }
+
+  return 0;
+  
+}
