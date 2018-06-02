3527d077d5fa5a5accc58da6f0e416faf1b43c9d
wildbartty <tominoz99@gmail.com>
2018-06-02 18:54:57 +1000

    added c++

---
+++ dumb.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+int main () {
+  clock_t start = clock(), diff;
+  long limit = 1l << 32;
+  for (unsigned long i = 0; i < limit; i++) {
+    if (i % 1000000 == 0) {
+      printf("%lu\n", i);
+    }
+  }
+
+  diff = clock() - start;
+
+  int msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
+  return 0;
+  
+}
