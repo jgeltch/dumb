ae8e833fd2c215b5ff62e103e969137887425045
Jake Geltch <jgeltch@gmail.com>
2018-06-17 19:35:24 +1000

    Added C# implementation

---
+++ dumb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+namespace dumb
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (long i = 1; i <= Math.Pow(2,32); i++)
+            {
+                if (i % 1000000 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds.ToString() + "ms total");
+        }
+    }
+}
